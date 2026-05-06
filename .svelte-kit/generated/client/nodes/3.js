cb78679a621e45daa417b71535d73272f5edc2ec
David londono <davidlo176@gmail.com>
2026-05-06 11:03:23 -0500

    update

---
+++ .svelte-kit/generated/client/nodes/3.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/bld-pairs/+page.svelte";
---
+++ .svelte-kit/generated/client/nodes/5.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/oll/practice/+page.svelte";
